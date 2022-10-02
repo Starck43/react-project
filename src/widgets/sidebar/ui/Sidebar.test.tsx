9a0fb5867098c54b72d72698992c1a84b63b136b
Starck43 <saloon.as@gmail.com>
2022-10-02 23:48:29 +0300

    added new Component for testing with Routers and fixed errors

--- src/widgets/sidebar/ui/Sidebar.test.tsx
+++ src/widgets/sidebar/ui/Sidebar.test.tsx
@@ -1,6 +1,6 @@
 import {fireEvent, screen} from "@testing-library/react"
 import {withTranslation} from "react-i18next"
-import {RenderWithTranslation} from "shared/lib/tests/render-with-translation/RenderWithTranslation"
+import {ComponentRender} from "shared/lib/tests/component-render/ComponentRender"
 
 import {Sidebar} from "./Sidebar"
 
@@ -8,14 +8,20 @@ import {Sidebar} from "./Sidebar"
 describe("Sidebar Tests", () => {
     const SidebarWithTranslation = withTranslation()(Sidebar)
     test("Sidebar in DOM", () => {
-        RenderWithTranslation(<SidebarWithTranslation />)
-        expect(screen.getByTestId("sidebar")).toBeInTheDocument()
+        ComponentRender(<SidebarWithTranslation />)
+        expect(screen.getByTestId("sidebar"))
+        .toBeInTheDocument()
     })
 
     test("Sidebar is collapsed", () => {
-        RenderWithTranslation(<SidebarWithTranslation />)
-        const toggleBtn = screen.getByTestId("sidebar-toggle")
-        fireEvent.click(toggleBtn)
-        expect(screen.getByTestId("sidebar")).toHaveClass("collapsed")
+        ComponentRender(<SidebarWithTranslation />)
+        const w = window.innerWidth
+        if (w < 992) {
+            const toggleBtn = screen.getByTestId("sidebar-toggle")
+            fireEvent.click(toggleBtn)
+            expect(screen.getByTestId("sidebar"))
+            .toHaveClass("collapsed")
+        }
+        screen.debug()
     })
 })
