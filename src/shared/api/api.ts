ee031faada511a923f3985de88e7df5c797fd361
Starck43 <saloon.as@gmail.com>
2022-11-20 15:37:52 +0200

    added interceptor for getting key before main request

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,5 +4,12 @@ import {AUTH_USER_KEY} from "shared/const/localStorage"
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {authorization: localStorage.getItem(AUTH_USER_KEY) || ""}, // just only for json-server
+})
+
+    // just only for json-server
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(AUTH_USER_KEY) || ""
+    }
+    return config
 })
