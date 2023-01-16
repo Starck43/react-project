95f3d012fdff1ea1eca2e08057d763821d858145
Starck43 <saloon.as@gmail.com>
2023-01-17 01:21:16 +0200

    v1.0.5 fixed error with reading non-existing folder, add Chromatic CI in workflow

--- scripts/generate-screenshots-report.js
+++ scripts/generate-screenshots-report.js
@@ -1,5 +1,5 @@
 const { promisify } = require("util")
-const { readdir, writeFile } = require("fs")
+const { fs, readdir, writeFile } = require("fs")
 const { join: joinPath, relative } = require("path")
 
 const asyncReaddir = promisify(readdir)
@@ -11,21 +11,26 @@ const expectedDir = joinPath(lokiDir, "reference")
 const diffDir = joinPath(lokiDir, "difference")
 
 ;(async function main() {
-    const diffs = await asyncReaddir(diffDir)
+    if (fs.existsSync(diffDir)) {
+        const diffs = await asyncReaddir(diffDir)
 
-    await writeFileAsync(
-        joinPath(lokiDir, "report.json"),
-        JSON.stringify({
-            newItems: [],
-            deletedItems: [],
-            passedItems: [],
-            failedItems: diffs,
-            expectedItems: diffs,
-            actualItems: diffs,
-            diffItems: diffs,
-            actualDir: relative(lokiDir, actualDir),
-            expectedDir: relative(lokiDir, expectedDir),
-            diffDir: relative(lokiDir, diffDir),
-        }),
-    )
+        await writeFileAsync(
+            joinPath(lokiDir, "report.json"),
+            JSON.stringify({
+                newItems: [],
+                deletedItems: [],
+                passedItems: [],
+                failedItems: diffs,
+                expectedItems: diffs,
+                actualItems: diffs,
+                diffItems: diffs,
+                actualDir: relative(lokiDir, actualDir),
+                expectedDir: relative(lokiDir, expectedDir),
+                diffDir: relative(lokiDir, diffDir),
+            }),
+        )
+    } else {
+        // eslint-disable-next-line no-console
+        console.log("Loki screenshots not found")
+    }
 })()
